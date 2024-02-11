60570642c99fe356855d214ca8f30601d18cb898
Muhammad Raza <muhammad.raza@matillion.com>
2024-02-11 15:49:15 +0000

    Implemented Scan function that stores items in the basket

--- pkg/checkout.go
+++ pkg/checkout.go
@@ -5,14 +5,30 @@ type ICheckout interface {
 	GetTotalPrice() int
 }
 
-type Checkout struct{}
-
-func NewCheckout() *Checkout {
-	return &Checkout{}
+type Checkout struct {
+	basket map[string]*int
 }
 
+// NewCheckout creates instance of Checkout
+func NewCheckout() *Checkout {
+	return &Checkout{
+		basket: map[string]*int{},
+	}
+}
+
+// Scan adds item to map of item and quantity. If item already exists in the map, then the quantity
+// is incremented. Since we are storing the quantity reference in the map, we can just increment the value without
+// storing it again in the map.
 func (c *Checkout) Scan(item string) {
-	// TODO implement me
+	if q, ok := c.basket[item]; ok {
+		*q++
+	} else {
+		quantity := 1
+		if _, ok := Items[item]; ok {
+			c.basket[item] = &quantity
+		}
+
+	}
 }
 
 func (c *Checkout) GetTotalPrice() int {
