ed5a65a2e59881139a10c58ef1f1b2ef1cf8da5c
Muhammad Raza <muhammad.raza@matillion.com>
2024-02-11 16:07:50 +0000

    Implemented GetTotalPrice function. All tests passing

--- Readme.md
+++ Readme.md
@@ -6,7 +6,11 @@
 - Item only supports one offer i.e. `3 for 130`. There is room to improve it and add more offer types e.g. `Meal Deal`
   or `3 for the price of 2` e.t.c
 - Unit price is detached from offer price. e.g. if the unit price is changed, the offer can become invalid. This can be
-  linked by introducing percentage reduction in price. 
+  linked by introducing percentage reduction in price.
+- To avoid calculating total price again and again. we can add `totalPrice` and a `priceChanged` attributes
+  to `Checkout` struct. We can then return `totalPrice` unless `priceChanged` is true. If `priceChanged` is true, then
+  calculate price again. `priceChanged` flag can be set in `Scan` function.
+- Nice to have a cli for visual purpose.
 
 ### Running test
 
--- pkg/checkout.go
+++ pkg/checkout.go
@@ -27,11 +27,26 @@ func (c *Checkout) Scan(item string) {
 		if _, ok := Items[item]; ok {
 			c.basket[item] = &quantity
 		}
-
 	}
 }
 
+// GetTotalPrice loops through basket and add item price to the total value. If offer exists for the particular item,
+// then add the discounted price. e.g. if `A` cost 50, and we add 7 'A's to the basket, and there is an offer that
+// 3 'A's will cost 130. We will add 120 to the total and deduce 3 from 7 quantity in the basket. Keep repeating this
+// process until the remaining quantity is less than the offer quantity. In this example, remainder will be 1 which is
+// less than offer. Then add full price of 1 'A' to the total. The total after offer should be 310.
 func (c *Checkout) GetTotalPrice() int {
-	// TODO implement me
-	return -1
+	total := 0
+	for k, v := range c.basket {
+		quantity := *v
+		desc := Items[k]
+		if desc.Offer != nil {
+			for quantity >= desc.Offer.Quantity {
+				total = total + desc.Offer.Price
+				quantity = quantity - desc.Offer.Quantity
+			}
+		}
+		total = total + (desc.UnitPrice * quantity)
+	}
+	return total
 }
